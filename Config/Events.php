09993d42ea117fb1afdd00edaf1278691fceac74
basic-app <dev@basic-app.com>
2020-02-23 22:49:42 +0300

    n/a

--- Config/Events.php
+++ Config/Events.php
@@ -5,7 +5,6 @@
  * @link http://basic-app.com
  */
 use BasicApp\System\SystemEvents;
-use BasicApp\Site\SiteEvents;
 use BasicApp\Member\Filters\MemberFilter;
 use BasicApp\Helpers\Url;
 
@@ -15,21 +14,3 @@ SystemEvents::onFilters(function($event)
 
     $event->filters['memberIsLoggedIn'] = ['before' => ['/member/', '/member/*']];
 });
-
-if (class_exists(SiteEvents::class))
-{
-    SiteEvents::onAccountMenu(function($event)
-    {
-        $user = service('user');
-
-        if ($user->getUser())
-        {
-            $event->items = [
-                'member' => [
-                    'label' => t('user', 'My Account'),
-                    'url' => Url::createUrl('member')
-                ]
-            ];
-        }
-    });
-}
